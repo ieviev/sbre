e451c803ceccf326406c90d6df7a3d11a1b05e5b
ieviev <ieviev@users.noreply.github.com>
2023-11-18 04:20:30 +0200

    native test

--- src/libsbre/Library.fs
+++ src/libsbre/Library.fs
@@ -3,17 +3,14 @@
 open System.Runtime.InteropServices
 open System
 
-
 [<UnmanagedCallersOnly(EntryPoint = "print_matches")>]
-let print_matches (pPattern: nativeint) (pInput: nativeint) : int =
+let print_matches (pStrPattern: nativeint) (pStrInput: nativeint) : int =
     try
-        let pattern = Marshal.PtrToStringUTF8(pPattern)
-        let input = Marshal.PtrToStringUTF8(pInput)
-        stdout.WriteLine $"called fn"
-        stdout.WriteLine $"pattern: {pattern}"
-        stdout.WriteLine $"input: {input}"
+        let pattern = Marshal.PtrToStringUTF8(pStrPattern)
+        let input = Marshal.PtrToStringUTF8(pStrInput)
         let matchArray = Sbre.Regex(pattern).Matches(input) |> Seq.toArray
-        stdout.WriteLine $"matches: %A{matchArray}"
+        for m in matchArray do
+            stdout.WriteLine $"%A{m.Index}\t{m.Index + m.Length}\t{m.Value}"
         0
     with
     | _ -> -1
@@ -29,30 +26,7 @@ let matches (pPattern: nativeint) (pInput: nativeint) : nativeint =
             matches
             |> Seq.map (fun v -> v.Value)
             |> String.concat "\n"
-        let pResultStr = Marshal.StringToCoTaskMemUTF8(resultstr);
-        pResultStr
+        Marshal.StringToCoTaskMemUTF8(resultstr);
     with
     | _ -> -1
 
-[<UnmanagedCallersOnly(EntryPoint = "add")>]
-let Add (a: int) (b: int) : int =
-    a + b + b
-
-[<UnmanagedCallersOnly(EntryPoint = "write_line")>]
-let WriteLine (pString: nativeint) : int =
-    try
-        let str = Marshal.PtrToStringUTF8(pString)
-        Console.WriteLine("TEST")
-        Console.WriteLine(str)
-        0
-    with
-    | _ -> -1
-
-
-[<UnmanagedCallersOnly(EntryPoint = "write_hello")>]
-let WriteHello () : int =
-    try
-        Console.WriteLine("hello")
-        0
-    with
-    | _ -> -1
